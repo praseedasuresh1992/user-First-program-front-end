6a462946804efeebde5398b1aa6178772690079b
praseedasuresh1992 <praseeeda@gmail.com>
2025-10-31 16:49:43 +0530

    dot env file added and component updated

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const apiUrl=import.meta.env.VITE_API_URL
 const api=axios.create({
-    baseURL:"http://localhost:8000/api/",withCredentials:true
+    baseURL:apiUrl ,
+    withCredentials:true
 })
 
 export default api
